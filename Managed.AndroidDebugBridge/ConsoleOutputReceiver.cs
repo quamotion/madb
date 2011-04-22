c90d59b9e49dbf0e6313608437807a9fe429b3bf
camalot <camalot@gmail.com>
2011-04-22 20:22:53 +0000

    added a default instance for the consoleoutput receiver
    git-svn-id: https://madb.svn.codeplex.com/svn@72151 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Managed.AndroidDebugBridge/ConsoleOutputReceiver.cs
+++ Managed.AndroidDebugBridge/ConsoleOutputReceiver.cs
@@ -5,7 +5,15 @@ using System.Text;
 
 namespace Managed.Adb {
 	public class ConsoleOutputReceiver : MultiLineReceiver {
-
+		private static ConsoleOutputReceiver _instance = null;
+		public static ConsoleOutputReceiver Instance {
+			get {
+				if ( _instance == null ) {
+					_instance = new ConsoleOutputReceiver ( );
+				}
+				return _instance;
+			}
+		}
 			protected override void ProcessNewLines ( string[] lines ) {
 				foreach ( var line in lines ) {
 					if ( String.IsNullOrEmpty ( line ) || line.StartsWith ( "#" ) ) {
