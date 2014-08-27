c15eff620e56e7559cb9c0b9a8aad5132a663b17
camalot <camalot@gmail.com>
2014-08-27 15:52:22 +0000

    more work on the site layout and content.
    git-svn-id: https://madb.svn.codeplex.com/svn@110342 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Madb.Site/Controllers/AssetsController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Madb.Site.Controllers {
+	public class AssetsController : Controller {
+		// GET: Assets
+		public PartialViewResult Gist(string id) {
+			return PartialView(model: id);
+		}
+	}
+}
---
+++ Madb.Site/Extensions/HtmlHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Camalot.Common.Extensions;
+
+namespace Madb.Site.Extensions {
+	public static partial class MadbSiteExtensions {
+		public static IHtmlString SiteName(this HtmlHelper helper) {
+			var value = ConfigurationManager.AppSettings["site:Name"].Or("");
+			return new MvcHtmlString(value);
+		}
+	}
+}
