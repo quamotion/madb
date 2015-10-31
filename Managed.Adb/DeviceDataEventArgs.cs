bdb6a0c9060ba937b8af16ffaf7de40defb11dd5
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-10-31 11:31:24 +0100

    Add missing file

---
+++ Managed.Adb/DeviceDataEventArgs.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="DeviceEventArgs.cs" company="The Android Open Source Project, Ryan Conrad, Quamotion">
+// Copyright (c) The Android Open Source Project, Ryan Conrad, Quamotion. All rights reserved.
+// </copyright>
+
+namespace Managed.Adb
+{
+    using System;
+
+    public class DeviceDataEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceDataEventArgs"/> class.
+        /// </summary>
+        /// <param name="device">The device.</param>
+        public DeviceDataEventArgs(DeviceData device)
+        {
+            this.Device = device;
+        }
+
+        /// <summary>
+        /// Gets the device.
+        /// </summary>
+        /// <value>The device.</value>
+        public DeviceData Device { get; private set; }
+    }
+}
