b5f2b87bfa7841a935fe9da1ec2106afd98a0f09
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-11-02 13:57:35 +0100

    DeviceMonitor: Add unit tests for all cases (100% coverage).

--- Managed.Adb.Tests/DeviceMonitorTests.cs
+++ Managed.Adb.Tests/DeviceMonitorTests.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Text;
 
 namespace Managed.Adb.Tests
 {
@@ -33,10 +30,114 @@ namespace Managed.Adb.Tests
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorNullTest()
+        {
+            DeviceMonitor monitor = new DeviceMonitor(null);
+        }
+
+        [TestMethod]
+        public void DeviceDisconnectedTest()
+        {
+            using (DeviceMonitor monitor = new DeviceMonitor(this.Socket))
+            {
+                DeviceMonitorSink sink = new DeviceMonitorSink(monitor);
+
+                Assert.AreEqual(0, monitor.Devices.Count);
+
+                // Start the monitor, detect the initial device.
+                base.RunTest(
+                OkResponse,
+                ResponseMessages("169.254.109.177:5555\tdevice\n"),
+                Requests("host:track-devices"),
+                () =>
+                {
+                    monitor.Start();
+
+                    Assert.AreEqual(1, monitor.Devices.Count);
+                    Assert.AreEqual(1, sink.ConnectedEvents.Count);
+                    Assert.AreEqual(0, sink.ChangedEvents.Count);
+                    Assert.AreEqual(0, sink.DisconnectedEvents.Count);
+                });
+
+                this.Socket.ResponseMessages.Clear();
+                this.Socket.Responses.Clear();
+                this.Socket.Requests.Clear();
+
+                // Device disconnects
+                var eventWaiter = sink.CreateEventSignal();
+
+                base.RunTest(
+                NoResponses,
+                ResponseMessages(""),
+                Requests(),
+                () =>
+                {
+                    eventWaiter.WaitOne(1000);
+                    Assert.AreEqual(0, monitor.Devices.Count);
+                    Assert.AreEqual(1, sink.ConnectedEvents.Count);
+                    Assert.AreEqual(0, sink.ChangedEvents.Count);
+                    Assert.AreEqual(1, sink.DisconnectedEvents.Count);
+                    Assert.AreEqual("169.254.109.177:5555", sink.DisconnectedEvents[0].Device.Serial);
+                });
+            }
+        }
+
+        [TestMethod]
+        public void DeviceConnectedTest()
+        {
+            using (DeviceMonitor monitor = new DeviceMonitor(this.Socket))
+            {
+                DeviceMonitorSink sink = new DeviceMonitorSink(monitor);
+
+                Assert.AreEqual(0, monitor.Devices.Count);
+
+                // Start the monitor, detect the initial device.
+                base.RunTest(
+                OkResponse,
+                ResponseMessages(""),
+                Requests("host:track-devices"),
+                () =>
+                {
+                    monitor.Start();
+
+                    Assert.AreEqual(0, monitor.Devices.Count);
+                    Assert.AreEqual(0, sink.ConnectedEvents.Count);
+                    Assert.AreEqual(0, sink.ChangedEvents.Count);
+                    Assert.AreEqual(0, sink.DisconnectedEvents.Count);
+                });
+
+                this.Socket.ResponseMessages.Clear();
+                this.Socket.Responses.Clear();
+                this.Socket.Requests.Clear();
+
+                // Device disconnects
+                var eventWaiter = sink.CreateEventSignal();
+
+                base.RunTest(
+                NoResponses,
+                ResponseMessages("169.254.109.177:5555\tdevice\n"),
+                Requests(),
+                () =>
+                {
+                    eventWaiter.WaitOne(1000);
+
+                    Assert.AreEqual(1, monitor.Devices.Count);
+                    Assert.AreEqual(1, sink.ConnectedEvents.Count);
+                    Assert.AreEqual(0, sink.ChangedEvents.Count);
+                    Assert.AreEqual(0, sink.DisconnectedEvents.Count);
+                    Assert.AreEqual("169.254.109.177:5555", sink.ConnectedEvents[0].Device.Serial);
+                });
+            }
+        }
+
+        [TestMethod]
         public void StartInitialDeviceListTest()
         {
             using (DeviceMonitor monitor = new DeviceMonitor(this.Socket))
             {
+                DeviceMonitorSink sink = new DeviceMonitorSink(monitor);
+
                 Assert.AreEqual(0, monitor.Devices.Count);
 
                 base.RunTest(
@@ -49,6 +150,60 @@ namespace Managed.Adb.Tests
 
                     Assert.AreEqual(1, monitor.Devices.Count);
                     Assert.AreEqual("169.254.109.177:5555", monitor.Devices.ElementAt(0).Serial);
+                    Assert.AreEqual(1, sink.ConnectedEvents.Count);
+                    Assert.AreEqual("169.254.109.177:5555", sink.ConnectedEvents[0].Device.Serial);
+                    Assert.AreEqual(0, sink.ChangedEvents.Count);
+                    Assert.AreEqual(0, sink.DisconnectedEvents.Count);
+                });
+            }
+        }
+
+        [TestMethod]
+        public void DeviceChangedTest()
+        {
+            using (DeviceMonitor monitor = new DeviceMonitor(this.Socket))
+            {
+                DeviceMonitorSink sink = new DeviceMonitorSink(monitor);
+
+                Assert.AreEqual(0, monitor.Devices.Count);
+
+                // Start the monitor, detect the initial device.
+                base.RunTest(
+                OkResponse,
+                ResponseMessages("169.254.109.177:5555\toffline\n"),
+                Requests("host:track-devices"),
+                () =>
+                {
+                    monitor.Start();
+
+                    Assert.AreEqual(1, monitor.Devices.Count);
+                    Assert.AreEqual(DeviceState.Offline, monitor.Devices.ElementAt(0).State);
+                    Assert.AreEqual(1, sink.ConnectedEvents.Count);
+                    Assert.AreEqual(0, sink.ChangedEvents.Count);
+                    Assert.AreEqual(0, sink.DisconnectedEvents.Count);
+                });
+
+                this.Socket.ResponseMessages.Clear();
+                this.Socket.Responses.Clear();
+                this.Socket.Requests.Clear();
+
+                // Device disconnects
+                var eventWaiter = sink.CreateEventSignal();
+
+                base.RunTest(
+                NoResponses,
+                ResponseMessages("169.254.109.177:5555\tdevice\n"),
+                Requests(),
+                () =>
+                {
+                    eventWaiter.WaitOne(1000);
+
+                    Assert.AreEqual(1, monitor.Devices.Count);
+                    Assert.AreEqual(DeviceState.Online, monitor.Devices.ElementAt(0).State);
+                    Assert.AreEqual(1, sink.ConnectedEvents.Count);
+                    Assert.AreEqual(1, sink.ChangedEvents.Count);
+                    Assert.AreEqual(0, sink.DisconnectedEvents.Count);
+                    Assert.AreEqual("169.254.109.177:5555", sink.ChangedEvents[0].Device.Serial);
                 });
             }
         }
