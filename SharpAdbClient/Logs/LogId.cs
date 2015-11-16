5017dff8e3fda9b01ba4318e82089676df8cfbcf
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-11-16 02:41:26 +0100

    Add LogId enumeration, which defines the IDs of the various Android logs

---
+++ SharpAdbClient/Logs/LogId.cs
@@ -0,0 +1,13 @@
+﻿namespace SharpAdbClient.Logs
+{
+    // https://android.googlesource.com/platform/system/core/+/master/include/log/log.h#596
+    public enum LogId
+    {
+        Main = 0,
+        Radio = 1,
+        Events = 2,
+        System = 3,
+        Crash = 4,
+        Kernel = 5
+    }
+}
