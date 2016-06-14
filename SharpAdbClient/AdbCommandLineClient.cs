291c8b5f22341ad70af46d4cffccd15321760221
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-06-15 00:44:19 +0200

    Remove IProgress from the SyncService, it is not implemented.

--- SharpAdbClient/AdbCommandLineClient.cs
+++ SharpAdbClient/AdbCommandLineClient.cs
@@ -41,8 +41,13 @@ namespace SharpAdbClient
                 throw new ArgumentNullException(nameof(adbPath));
             }
 
+#if NETSTANDARD1_5
             bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
             bool isUnix = RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+#else
+            bool isWindows = Environment.OSVersion.Platform == PlatformID.Win32NT;
+            bool isUnix = Environment.OSVersion.Platform == PlatformID.MacOSX || Environment.OSVersion.Platform == PlatformID.Unix;
+#endif
 
             if (isWindows)
             {
