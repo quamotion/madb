8047e32f6641627b31268ef831e1aedabda7857a
Patrick Krämer <patagonahn@gmail.com>
2022-06-10 01:03:27 +0200

    fix encoding

--- SharpAdbClient/AdbSocket.cs
+++ SharpAdbClient/AdbSocket.cs
@@ -145,9 +145,9 @@ namespace SharpAdbClient
                 throw new ArgumentNullException(nameof(path));
             }
 
-            this.SendSyncRequest(command, path.Length);
-
             byte[] pathBytes = AdbClient.Encoding.GetBytes(path);
+            this.SendSyncRequest(command, pathBytes.Length);
+
             this.Write(pathBytes);
         }
 
